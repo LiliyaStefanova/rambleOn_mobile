63b820a3e929bae37cf320a631e407eaf67380b5
elsteremonstere <liliya.stefanova@gmail.com>
2020-09-09 21:27:26 +0100

    introduce typescript

---
+++ App.ts
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}/>
+  );
+}
+
+const styles:any = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  }
+});
